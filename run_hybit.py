7e95d9721966daf804d427c5c9d4f1cd30543c94
Danila Valko <d.v.valko@gmail.com>
2024-11-26 17:01:29 +0100

    modified:   simulators/pv/pvgis.py

---
+++ run_hybit.py
@@ -0,0 +1,11 @@
+import os
+import itertools
+
+os.system(f"python grid_model.py")
+os.system(f"python grid_profiles.py")
+alg = ['default', 'cohda', 'swarm']
+for i, j in itertools.product(alg, repeat=2):
+    print(f"python hybit.py --within {i} --between {j}")
+    os.system(f"python hybit.py --within {i} --between {j}")
+
+# outputs are in the results folder 
